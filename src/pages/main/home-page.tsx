1b66178474c46c0274ac704469819ea957519695
jackbeav <jackbeaver0+github@gmail.com>
2024-12-31 14:12:51 -0700

    feat: pull items from fake store api

--- src/pages/main/home-page.tsx
+++ src/pages/main/home-page.tsx
@@ -1,77 +1,47 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
-interface Item {
-  name:string,
-  seller:string,
-  price:number,
-  rating:number
+interface Item{
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  rating: {
+    rate: number;
+    count: number;
+  }
+  image: string;
 }
 
 export default function HomePage() {
-  const [featured, setFeatured] = useState<Item[]>([
-    {
-      name:"example1",
-      seller:"seller1",
-      price:10.50,
-      rating:4.5
-    },
-    {
-      name:"example2",
-      seller:"seller1",
-      price:10.50,
-      rating:4.5
-    },
-    {
-      name:"example3",
-      seller:"seller2",
-      price:10.50,
-      rating:4.5
-    },
-    {
-      name:"example2",
-      seller:"seller1",
-      price:10.50,
-      rating:4.5
-    },
-    {
-      name:"example2",
-      seller:"seller1",
-      price:10.50,
-      rating:4.5
-    },
-    {
-      name:"example2",
-      seller:"seller1",
-      price:10.50,
-      rating:4.5
-    },
-    {
-      name:"example2",
-      seller:"seller1",
-      price:10.50,
-      rating:4.5
-    }
-  ])
-  const [popular, setPopular] = useState<Item[]>([
-    {
-      name:"popular1",
-      seller:"seller1",
-      price:10.50,
-      rating:4.5
-    }
-  ])
-  const [forYou, setForYou] = useState<Item[]>([
-    {
-      name:"foryou1",
-      seller:"seller1",
-      price:10.50,
-      rating:4.5
-    }
-  ])
+  const [featured, setFeatured] = useState<Item[]>([])
+  const [popular, setPopular] = useState<Item[]>([])
+  const [forYou, setForYou] = useState<Item[]>([])
 
   useEffect(() => {
     //TODO: fill in product lists with backend API calls
+    axios.get("https://fakestoreapi.com/products?limit=10")
+    .then((response)=>{
+      setFeatured(response.data)
+      setPopular(response.data)
+      setForYou(response.data)
+    })
   }, [])
+
+  const listItems = (itemList:Item[]) => {
+    return(
+      <div className="flex flex-nowrap overflow-auto">
+        {itemList.map((item:Item, index:number) => (
+          <div key = {index} className="m-4 flex-none">
+            <img src={item.image} alt={item.title} className="w-48 h-48"/>
+            <p className="w-48">{item.title}</p>
+            <p className="w-48">${item.price}</p>
+          </div>
+        ))}
+      </div>
+    )
+  }
   
   return (
     <div>
@@ -84,44 +54,17 @@ export default function HomePage() {
       {/* Product Lists */}
       <div className="flex flex-col w-full mx-auto">
         <h3>Featured</h3>
-        <div className="flex flex-nowrap overflow-x-auto">
-          {featured.map((item:Item, index:number) => (
-            <div key = {index} className="m-4 flex-none">
-              {/* TODO: replace this with a standardized product component later */}
-              <p>{item.name}</p>
-              <p>By {item.seller}</p>
-              <p>${item.price}</p>
-            </div>
-          ))}
-        </div>
+        {listItems(featured)}
       </div>
-
       <div className="flex flex-col w-full mx-auto">
         <h3>Popular</h3>
         <div className="flex flex-row flex-nowrap overflow-auto">
-          {popular.map((item:Item, index:number) => (
-            <div key = {index} className="m-4 flex-none">
-              {/* TODO: replace this with a standardized product component later */}
-              <p>{item.name}</p>
-              <p>By {item.seller}</p>
-              <p>${item.price}</p>
-            </div>
-          ))}
+          {listItems(popular)}
         </div>
       </div>
-
       <div className="flex flex-col w-full mx-auto">
         <h3>For You</h3>
-        <div className="flex flex-row flex-nowrap overflow-auto"> {/* ask how i can get this to scroll */}
-          {forYou.map((item:Item, index:number) => (
-            <div key = {index} className="m-4 flex-none">
-              {/* TODO: replace this with a standardized product component later */}
-              <p>{item.name}</p>
-              <p>By {item.seller}</p>
-              <p>${item.price}</p>
-            </div>
-          ))}
-        </div>
+        {listItems(forYou)}
       </div>
     </div>
   );
