fa5b88a124c042c6f7dcf630d8570df0aded555f
jackbeav <jackbeaver0+github@gmail.com>
2025-01-01 17:26:19 -0600

    feat: example hero segment and styling

--- src/pages/main/home-page.tsx
+++ src/pages/main/home-page.tsx
@@ -20,20 +20,42 @@ export default function HomePage() {
   const [forYou, setForYou] = useState<Item[]>([])
 
   useEffect(() => {
-    //TODO: fill in product lists with backend API calls
+    /*
     axios.get("https://fakestoreapi.com/products?limit=10")
     .then((response)=>{
       setFeatured(response.data)
       setPopular(response.data)
       setForYou(response.data)
     })
+    */
+
+    let dummyList:Item[] = []
+
+    for(let i = 0; i < 9; i++){
+      dummyList.push(
+        {
+          id: 1,
+          title: "example",
+          price: 10.50,
+          category: "cat",
+          description: "desc",
+          rating: {
+            rate: 5,
+            count: 21
+          },
+          image: ""
+        }
+      )
+    }
+
+    setFeatured(dummyList)
   }, [])
 
   const listItems = (itemList:Item[]) => {
     return(
-      <div className="flex flex-nowrap overflow-auto">
+      <div className="flex flex-nowrap overflow-auto gap-2">
         {itemList.map((item:Item, index:number) => (
-          <div key = {index} className="m-4 flex-none">
+          <div key = {index} className="flex-none bg-slate-100 rounded-lg p-2 ">
             <img src={item.image} alt={item.title} className="w-48 h-48"/>
             <p className="w-48">{item.title}</p>
             <p className="w-48">${item.price}</p>
@@ -44,27 +66,25 @@ export default function HomePage() {
   }
   
   return (
-    <div>
+    <div className="bg-white p-4">
       {/* hero section */}
       <div>
-        {/* image of some sort promoting deal, perhaps a carousel? */}
+        <img src="../landing-hero-example.png"/>
       </div>
       
 
       {/* Product Lists */}
-      <div className="flex flex-col w-full mx-auto">
-        <h3>Featured</h3>
+      <div className="flex flex-col w-full mx-auto ">
+        <p className="text-2xl font-bold py-4">Featured</p>
         {listItems(featured)}
       </div>
       <div className="flex flex-col w-full mx-auto">
-        <h3>Popular</h3>
-        <div className="flex flex-row flex-nowrap overflow-auto">
-          {listItems(popular)}
-        </div>
+        <p className="text-2xl font-bold py-4">Popular</p>
+        {listItems(featured)}
       </div>
       <div className="flex flex-col w-full mx-auto">
-        <h3>For You</h3>
-        {listItems(forYou)}
+        <p className="text-2xl font-bold py-4">For You</p>
+        {listItems(featured)}
       </div>
     </div>
   );
