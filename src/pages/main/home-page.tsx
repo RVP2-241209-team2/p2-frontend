302b9d45b6780d40524bfe657d150d0311854a96
jackbeav <jackbeaver0+github@gmail.com>
2025-01-01 22:08:00 -0600

    feat: navigate to products page

--- src/pages/main/home-page.tsx
+++ src/pages/main/home-page.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface Item{
   id: number;
@@ -20,21 +21,29 @@ export default function HomePage() {
   const [forYou, setForYou] = useState<Item[]>([])
 
   useEffect(() => {
+    loadItems()
+  }, [])
 
-    axios.get("https://fakestoreapi.com/products?limit=10")
+  const nav = useNavigate()
+
+  const loadItems = async () => {
+    await axios.get("https://fakestoreapi.com/products?limit=10")
     .then((response)=>{
       setFeatured(response.data)
       setPopular(response.data)
       setForYou(response.data)
     })
-    
-  }, [])
+  }
+
+  const navToItem = async (id:number) => {
+      nav("products/"+id)
+  }
 
   const listItems = (itemList:Item[]) => {
     return(
       <div className="flex flex-nowrap overflow-auto gap-2">
         {itemList.map((item:Item, index:number) => (
-          <div key = {index} className="flex-none bg-slate-100 rounded-lg p-2 ">
+          <div key = {index} className="flex-none bg-slate-100 rounded-lg p-2 " onClick={() => {navToItem(item.id)}}>
             <img src={item.image} alt={item.title} className="w-48 h-48"/>
             <p className="w-48">{item.title}</p>
             <p className="w-48">${item.price}</p>
