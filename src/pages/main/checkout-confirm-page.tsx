743656031586224f1a726b8999c2b7a5bfad9e4a
KadenMK394 <kadenmk394@gmail.com>
2025-01-13 19:19:13 -0600

    created new page for checkout confirmation

---
+++ src/pages/main/checkout-confirm-page.tsx
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
+export default function CheckoutSuccess() {
+    const navigate = useNavigate();
+    
+    return(
+        <>
+        </>
+    )
+}
